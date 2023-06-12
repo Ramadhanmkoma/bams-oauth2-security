ea5f99af757ddb4f2dd5204d09c0c9560712f4a8
Ramadhan Mkoma <Ramadhanmkoma9@gmail.com>
2023-06-12 23:40:21 +0300

    clean secure configurations

--- src/main/java/com/biometric/bams/controller/StudentController.java
+++ src/main/java/com/biometric/bams/controller/StudentController.java
@@ -1,10 +1,14 @@
 package com.biometric.bams.controller;
 
+import com.biometric.bams.model.Student;
 import com.biometric.bams.repository.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@CrossOrigin("*")
 @RestController  //Respond to payloads incoming and outgoing as JSON REST endpoints
 @RequestMapping("/api/v1/students")     //Tells the router what mapping url will look like.
 public class StudentController {
@@ -12,4 +16,8 @@ public class StudentController {
     //Inject or autowire the students repository
     @Autowired
     private StudentRepo studentRepo;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Student> getStudents() { return studentRepo.findAll(); }
 }
